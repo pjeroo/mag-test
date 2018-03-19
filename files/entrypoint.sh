061831c482ef64497d8f374a3341be857c84cf4b
Sergey Makarov <s.makarov@vl.ru>
2018-03-19 10:25:02 +0700

    fix port in entrypoint

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,7 +3,7 @@
 CURRENT_IP=$(awk 'END{print $1}' /etc/hosts)
 
 sed -i "s/nginx/${HOSTNAME}/g" /var/www/html/index.nginx-debian.html
-sed -i "s/%nginx-child%/${CURRENT_IP}/g" /etc/haproxy/haproxy.cfg
+sed -i "s/%nginx-child%/${CURRENT_IP}:8080/g" /etc/haproxy/haproxy.cfg
 
 service nginx start
 service haproxy start

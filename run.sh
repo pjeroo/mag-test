43df7da39d1dea881d8275f292451bd02bde33d6
Sergey Makarov <s.makarov@vl.ru>
2018-03-19 14:35:49 +0700

    added swarm

--- run.sh
+++ run.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
-docker build . -t mag-test:latest
+docker swarm init
+docker service create --name mag-test --publish 80:80 --health-interval 5s --replicas 2 badfin/mag-test:latest
 
-docker network create --subnet=172.38.0.0/16 --ip-range=172.38.5.0/24 mag
+sleep 10
 
-docker run --name=mag-test1 --network=mag --ip=172.38.5.2 -d mag-test:latest
-docker run --name=mag-test2 --network=mag --ip=172.38.5.3 -d mag-test:latest
-
-sleep 1
-
-NGINX1_RESP=$(curl -s -o /dev/null -w "%{http_code}" 172.38.5.2:80)
-NGINX2_RESP=$(curl -s -o /dev/null -w "%{http_code}" 172.38.5.3:80)
-
-if [ "${NGINX1_RESP}" == 200 ]
-then
-    echo "CHECK NGINX 1 - ok"
-else
-    echo "CHECK NGINX 1 - failed. status code: ${NGINX1_RESP}"
-fi
-
-if [ "${NGINX2_RESP}" == 200 ]
-then
-    echo "CHECK NGINX 2 - ok"
-else
-    echo "CHECK NGINX 2 - failed. status code: ${NGINX2_RESP}"
-fi
+docker ps --format='{{.ID}}' --filter="name=mag-test" | xargs docker inspect \
+ --format='Status of {{.Name}} is {{.State.Health.Status}}'
--- src/files/entrypoint.sh
+++ src/files/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-CURRENT_IP=$(awk 'END{print $1}' /etc/hosts)
 NGINX_PORT=8080
 
 sed -i "s/nginx/${HOSTNAME}/g" /var/www/html/index.nginx-debian.html
-sed -i "s/%nginx-child%/${CURRENT_IP}:${NGINX_PORT}/g" /etc/haproxy/haproxy.cfg
+sed -i "s/%nginx-child%/localhost:${NGINX_PORT}/g" /etc/haproxy/haproxy.cfg
 sed -i "s/%nginx-port%/${NGINX_PORT}/g" /etc/nginx/sites-enabled/default
 
 service nginx start
